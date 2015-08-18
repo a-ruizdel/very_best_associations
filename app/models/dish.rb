b157af5b236d68043b8f262fdda7c20e4ad31bce
a-ruizdel <a-rdl@Arlenes-MacBook-Air.local>
2015-08-18 00:19:08 -0500

    completes neigborhood and venue association

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,7 @@ class Dish < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :cuisine_id, :presence => true
 
+  belongs_to :cuisine
+
+
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,6 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true
   validates :name, :uniqueness => { :scope => [:city] }
   validates :city, :presence => true
+
+  has_many :venues
 end
